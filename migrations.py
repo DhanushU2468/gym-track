5a77bea7575355c9e8e53fb29ec7b48261d6ad66
DhanushU2468 <danushumesh79@gmail.com>
2025-04-14 14:57:32 +0530

    ready for deploy

--- migrations.py
+++ migrations.py
@@ -1,10 +1,16 @@
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from app import app, db
+from flask_migrate import upgrade
 
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
+def migrate():
+    with app.app_context():
+        # Create/Upgrade database tables
+        upgrade()
+
 if __name__ == '__main__':
-    manager.run() 
+    migrate() 
